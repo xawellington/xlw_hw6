bca95bcfb576de6ce020d6aab45871b9dc7cf985
xawellington <158227468+xawellington@users.noreply.github.com>
2024-03-27 21:11:55 -0400

    Update Cow.cpp

--- Cow.cpp
+++ Cow.cpp
@@ -1,10 +1,23 @@
 #include "Cow.h"
 
-// TODO: write your code to initialize the static (const) member variable
+const string Cow::SAYS = "Moo!";
 
-// TODO: write your code of the constructor
+Cow::Cow(string name, int age, int weight) : name(name), age(age), weight(weight){}
 
 // TODO: write your code of the member functions (incrementAge(), setWeight(int weight), getAnimalType())
+void Cow::incrementAge(){
+  age++;
+}
+
+void Cow::setWeight(int weight){
+  this->weight = weight;
+
+string Cow::getAnimalType(){
+  return "Cow";
+}
 
 // TODO: write your code of the overloaded << operator
-
+ostream &operator<<(ostream &out, Cow &cow){
+    out<<"Name: " << cow.name << " Age: " << cow.age << " Weight: " << cow.weight;
+  return out;
+}
